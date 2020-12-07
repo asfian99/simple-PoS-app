818e447d307f5668a100bdeff2760fb597e0d23c
Asfian99 <mk.asfian@gmail.com>
2020-12-07 11:37:35 +0700

    added paid feature

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import List from "./components/List";
 function App() {
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
-  const [sum, setSum] = useState("");
+  const [sum, setSum] = useState(1);
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
+  const [paid, setPaid] = useState(0);
+  const [cashback, setCashback] = useState(0);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -25,7 +27,15 @@ function App() {
     setCart([...cart, newItem]);
     setName("");
     setPrice("");
-    setSum("");
+    setSum(1);
+  };
+
+  const handleConfirm = (e) => {
+    e.preventDefault();
+
+    if (paid >= total) {
+      setCashback(paid - total);
+    }
   };
 
   return (
@@ -35,21 +45,21 @@ function App() {
         <label htmlFor="name">nama barang</label>
         <input
           type="text"
-          name="name"
+          id="name"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
         <label htmlFor="price">harga</label>
         <input
           type="number"
-          name="price"
+          id="price"
           value={price}
           onChange={(e) => setPrice(e.target.value)}
         />
         <label htmlFor="sum">Jumlah</label>
         <input
           type="number"
-          name="sum"
+          id="sum"
           value={sum}
           onChange={(e) => setSum(e.target.value)}
         />
@@ -57,7 +67,20 @@ function App() {
       </form>
       <List items={cart} />
       <footer>
-        <h1>Jumlah : {total}</h1>
+        <form onSubmit={handleConfirm}>
+          <p>Total yang harus dibayar : Rp {total}</p>
+          <div>
+            <label htmlFor="paid">Uang Dibayarkan</label>
+            <input
+              type="number"
+              id="paid"
+              value={paid}
+              onChange={(e) => setPaid(e.target.value)}
+            />
+          </div>
+          <button type="submit">Konfirmasi</button>
+          <p>Total Kembalian : Rp {cashback}</p>
+        </form>
       </footer>
     </div>
   );
