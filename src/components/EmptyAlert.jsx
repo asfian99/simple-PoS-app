05d66e0fea3257266a7e26c8345d14987aee92e8
Asfian99 <mk.asfian@gmail.com>
2020-12-07 21:00:13 +0700

    added delete item & install react-router

---
+++ src/components/EmptyAlert.jsx
@@ -0,0 +1,8 @@
+const EmptyAlert = () => {
+  return (
+    <div className="w-1/3 mx-auto p-3 text-red-500 font-medium text-center bg-red-100 rounded-md">
+      <p>Keranjang Kosong</p>
+    </div>
+  );
+};
+export default EmptyAlert;
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,14 @@
-const List = ({ items }) => {
+const List = ({ items, deleteItem }) => {
   return (
     <>
       <table className="table-fixed w-full">
         <thead>
           <tr className="bg-gray-100">
-            <th className="p-2 w-3/6 border">Nama Barang</th>
+            <th className="p-2 w-5/12 border">Nama Barang</th>
             <th className="p-2 w-1/6 border">Harga Satuan</th>
             <th className="p-2 w-1/6 border">Jumlah Barang</th>
             <th className="p-2 w-1/6 border">Jumlah Harga</th>
+            <th className="p-2 w-1/12 border">Aksi</th>
           </tr>
         </thead>
         <tbody>
@@ -20,6 +21,14 @@ const List = ({ items }) => {
                 <td className="border p-2">{price}</td>
                 <td className="border p-2">{sum}</td>
                 <td className="border p-2">{sumPrice}</td>
+                <td className="border p-2 text-center">
+                  <button
+                    className="py-1 px-2 bg-red-100 hover:bg-red-500 font-medium text-red-500 hover:text-white rounded"
+                    onClick={() => deleteItem(id)}
+                  >
+                    Hapus
+                  </button>
+                </td>
               </tr>
             );
           })}
